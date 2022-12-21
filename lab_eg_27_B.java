059461610645f815c295f454c202ddcf0ad5727a
riteshdoit <120389851+riteshdoit@users.noreply.github.com>
2022-12-22 00:28:06 +0530

    Update lab_eg_27_B.java

--- lab_eg_27_B.java
+++ lab_eg_27_B.java
@@ -1,6 +1,10 @@
 // Java program to count occurrences
 // of an element
- 
+ 2) Count number of occurrences (or frequency) in a sorted array   
+               Input: arr[] = {1, 1, 2, 2, 2, 2, 3,},   x = 1
+               Output: 2  
+              Input: arr[] = {1, 1, 2, 2, 2, 2, 3,},   x = 4
+             Output: -1 // 4 doesn't occur in arr[]
 class lab_eg_27_B
 {
     // Returns number of times x occurs in arr[0..n-1]
