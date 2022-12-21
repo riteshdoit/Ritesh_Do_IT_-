f08f285778c353da951a333da70695cf486f6f0b
riteshdoit <120389851+riteshdoit@users.noreply.github.com>
2022-12-22 00:13:03 +0530

    Update lab_eg_11_A.java

--- lab_eg_11_A.java
+++ lab_eg_11_A.java
@@ -1,3 +1,5 @@
+-11.a) Write a program that takes three double values X0, V0, and t from the user and prints the value, where g is the constant 9.78033. This value is the displacement in meters after t seconds when an object is thrown straight up from initial position x0 at velocity v0 meters per second.
+
 import java.util.Scanner;
 
 public class lab_eg_11_A {
