90a2008f8d4768017db7dd008a477b0a58aeb8d7
riteshdoit <120389851+riteshdoit@users.noreply.github.com>
2022-12-22 00:16:52 +0530

    Update lab_eg_15.java

--- lab_eg_15.java
+++ lab_eg_15.java
@@ -1,3 +1,5 @@
+15 . Write a program to implement following inheritance. Accept data for 5 persons and display the name of employee having salary greater than 5000. Class Name: Person Member variables: Name, age Class Name: Employee Member variables: Designation, salary.
+
 class lab_eg_15{
     String name;
     int age;
