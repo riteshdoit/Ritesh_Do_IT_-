35f29f0730ce832d81dcd0a995918faf6231c0e0
riteshdoit <120389851+riteshdoit@users.noreply.github.com>
2022-12-22 00:20:30 +0530

    Update lab_eg_21.java

---
+++ lab_eg_21.java
@@ -0,0 +1,2 @@
+21.Create GUI using Swing.  As shown in the figure. Write a program to print the text entered in the text field-1 into text field-2 after button click. Display entered text in label below button also.
+
