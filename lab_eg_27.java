f776139858572706476944651b0671435ee28bbd
riteshdoit <120389851+riteshdoit@users.noreply.github.com>
2022-12-22 00:25:14 +0530

    Update lab_eg_27.java

--- lab_eg_27.java
+++ lab_eg_27.java
@@ -1,3 +1,11 @@
+27 .        1) Find Subarray with given sum       
+  Input:  arr[] = {1, 4, 20, 3, 10, 5}, sum = 33
+             Output: Sum found between indexes 2 and 4 
+             Explanation: Sum of elements between indices 2 and 4 is 20 + 3 + 10=33        
+             Input: arr[] = {1, 4}, sum = 0
+             Output: No subarray found
+             Explanation: There is no subarray with 0 sum    
+
 public class lab_eg_27 {
     /* Returns true if the there is a
 subarray of arr[] with a sum equal to
